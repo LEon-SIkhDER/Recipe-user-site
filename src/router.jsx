5395f56041ba80ee07992c4fa5dc44a363a94aa0
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-09-17 01:21:08 +0600

    header banner with responsive

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router";
+import Home from "./Components/Home";
+import Root from "./Layout/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>I am working</div>,
+    element:<Root></Root> ,
+    children:[
+      {index:true,Component:Home}
+    ]
   },
 ]);
