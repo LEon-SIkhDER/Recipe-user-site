62ed0dddec4196034948b285382f814f200baaed
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-09-21 01:54:53 +0600

    bug solved

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,8 +1,9 @@
 import React, { use, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router';
 import { AuthContext } from '../AuthContext/AuthContext';
-import { updateProfile } from 'firebase/auth';
+import { signInWithPopup, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebase.init';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 const Register = () => {
     const [loading, setLoading] = useState(false)
@@ -10,6 +11,8 @@ const Register = () => {
     const { createUser } = use(AuthContext)
     const navigate = useNavigate()
     const location = useLocation()
+
+    const provider = new GoogleAuthProvider()
 
 
 
@@ -22,12 +25,12 @@ const Register = () => {
         const photoUrl = e.target.photoUrl.value
         const email = e.target.email.value
         const password = e.target.password.value
-        if(password.length < 6){
+        if (password.length < 6) {
             setError("Password must be at least 6 characters")
             setLoading(false)
             return
         }
-        else if(!/[A-Z]/.test(password)){
+        else if (!/[A-Z]/.test(password)) {
             setError("Password must have one Uppercase")
 
             setLoading(false)
@@ -35,7 +38,7 @@ const Register = () => {
 
 
         }
-        else if(!/[a-z]/.test(password)){
+        else if (!/[a-z]/.test(password)) {
             setError("Password must have one Lowercase")
 
             setLoading(false)
@@ -111,6 +114,19 @@ const Register = () => {
 
     }
 
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result)
+                if (location.state) {
+                    navigate(location.state)
+                }
+                else {
+                    navigate("/")
+                }
+            })
+    }
+
     return (
         <div className='mb-20 mt-10'>
             <section>
@@ -119,13 +135,13 @@ const Register = () => {
                         <h1 className="text-4xl font-bold">Register now!</h1>
                         <form onSubmit={handleRegister} className="fieldset">
                             <label className="label text-black font-semibold">Name</label>
-                            <input type="text" className="input" placeholder="Name" name='name' required/>
+                            <input type="text" className="input" placeholder="Name" name='name' required />
                             <label className="label text-black font-semibold">Photo Url</label>
-                            <input type="text" className="input" placeholder="Photo Url" name='photoUrl' required/>
+                            <input type="text" className="input" placeholder="Photo Url" name='photoUrl' required />
                             <label className="label text-black font-semibold">Email</label>
                             <input type="email" className="input" placeholder="Email" name='email' required />
                             <label className="label text-black font-semibold">Password</label>
-                            <input type="password" className="input" placeholder="Password" name='password' required/>
+                            <input type="password" className="input" placeholder="Password" name='password' required />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             {error && <p className='text-red-500 '>{error}</p>}
                             <h1 className='text-sm font-semibold text-black '>Already Have An Account? <Link to={"/signin"} className='text-pink-600 underline'>LogIn</Link></h1>
@@ -136,7 +152,7 @@ const Register = () => {
                                 <div className='border-b border-gray-300 w-full'></div>
                             </div>
                         </form>
-                        <button className="btn bg-white text-black border-[#e5e5e5]">
+                        <button onClick={handleGoogleLogin} className="btn bg-white text-black border-[#e5e5e5]">
                             <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                             Login with Google
                         </button>
